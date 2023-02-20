ffa232ee3bc28ffe07215b8ece7727212793d8aa
NicolasOe <nicolasoe@hotmail.com>
2023-02-20 15:42:01 -0300

    Janken maybe lol

--- src/Games/Jankenpo/Hooks/useJankenpo.ts
+++ src/Games/Jankenpo/Hooks/useJankenpo.ts
@@ -1,11 +1,45 @@
 import React from "react"
 
 interface JankenpoReturn {
-    runTurn: () => void
+    runTurn: (playerMove: JankenpoMove) => void
 }
 
-export const useJankenpo = (): JankenpoReturn  => {
-    const runTurn = () => { console.log("Yaaay") }
+enum JankenpoMove {
+    GU = 0,
+    CHOKI = 1,
+    PA = 2
+}
+
+enum JankenpoGameStateEnum {
+    P1WIN = "p1win",
+    P2WIN = "p2win",
+    DRAW = "draw"
+}
+
+// Temporary function, to insert IA
+const runIAnMove = (): JankenpoMove => {
+    return JankenpoMove.CHOKI;
+}
+
+const useJankenpo = (): JankenpoReturn  => {
+    function validateWin(winnerCandidateMove: JankenpoMove, loserCandidateMove: JankenpoMove) {
+        return (winnerCandidateMove + 1) % 3 === loserCandidateMove;
+    }
+    function winnerCheck(p1Move: JankenpoMove, p2Move: JankenpoMove): JankenpoGameStateEnum {
+        if (validateWin(p1Move, p2Move)) {
+            return JankenpoGameStateEnum.P1WIN
+        }
+        if (validateWin(p2Move, p1Move)) {
+            return JankenpoGameStateEnum.P2WIN
+        }
+        return JankenpoGameStateEnum.DRAW
+    }
+
+    const runTurn = (playerMove: JankenpoMove) => { 
+        return winnerCheck(playerMove, runIAnMove());
+    }
 
     return { runTurn }
 }
+
+export default (useJankenpo);
